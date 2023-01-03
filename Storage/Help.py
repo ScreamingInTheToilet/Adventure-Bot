9a23cb154b0bf9cea5b49c0df973d0ad72c8d87d
ScreamingInTheToilet <121836498+ScreamingInTheToilet@users.noreply.github.com>
2023-01-03 11:27:43 +0700

    Add files via upload

---
+++ Storage/Help.py
@@ -0,0 +1,66 @@
+Cmd = [{"Command": "8ball", "info": ",8ball <Question>", "Requirements": "None"
+        },
+       {"Command": "ping", "info": ",ping",
+        "Requirements": "None"
+        },
+       {"Command": "clear", "info": ",clear <amount of text>",
+        "Requirements": "Manage Messages permission"
+        },
+       {"Command": "kick", "info": ",kick <member>",
+        "Requirements": "Kick Members Permission"
+        },
+       {"Command": "tempban", "info": ",tempban <member> <duration>",
+        "Requirements": "Ban Members permission"
+        },
+       {"Command": "unban", "info": ",unban <member>",
+        "Requirements": "Ban Members permission"
+        },
+       {"Command": "bj", "info": ",bj",
+        "Requirements": "None"
+        },
+       {"Command": "profile", "info": ",profile <member:none>",
+        "Requirements": "None"
+        },
+       {"Command": "starter", "info": ",starter",
+        "Requirements": "None"
+        },
+       {"Command": "weaponshop", "info": ",weaponshop",
+        "Requirements": "None"
+        },
+       {"Command": "sell", "info": ",sell <weapon>",
+        "Requirements": "None"
+        },
+       {"Command": "travel", "info": ",travel <x coord> <y coord>",
+        "Requirements": "None"
+        },
+       {"Command": "fight", "info": ",fight <enemy>",
+        "Requirements": "None"
+        },
+       {"Command": "inventory", "info": ",inventory",
+        "Requirements": "None"
+        },
+       {"Command": "upgrade", "info": ",upgrade <weapon>",
+        "Requirements": "None"
+        },
+       {"Command": "gift", "info": ",gift <amount> <item> <member>",
+        "Requirements": "None"
+        },
+       {"Command": "market", "info": ",market <item> <price> <amount>",
+        "Requirements": "None"
+        },
+       {"Command": "buy", "info": ",buy <item> <amount>",
+        "Requirements": "None"
+        },
+       {"Command": "marketinfo", "info": ",marketinfo",
+       "Requirements": "None"
+        },
+       {"Command": "shop", "info": ",shop",
+        "Requirements": "None"
+        },
+       {"Command": "search", "info": ",search",
+        "Requirements": "5 seconds cooldown per usage"
+        },
+       {"Command": "fish", "info": ",fish", 
+       "Requirements": "A fishing rod is needed."
+       },
+       ]
